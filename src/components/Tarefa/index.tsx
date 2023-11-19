9e4071d76e844d2ae375d6dd9757d7d35132b618
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-19 19:12:06 -0300

    Criar o estado de troca de botoes

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da Tarefa</S.Titulo>
-    <S.Tag>Importante</S.Tag>
-    <S.Tag>Pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
+  const [editando, setEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag>{prioridade}</S.Tag>
+      <S.Tag>{status}</S.Tag>
+      <S.Descricao placeholder={descricao} />
+      <S.BarraAcoes>
+        {editando ? (
+          <>
+            <S.Botao onClick={() => setEditando(false)}>Salvar</S.Botao>
+            <S.Botao>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEditando(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,27 +1,43 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendentes'
+  },
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendentes'
+  },
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendentes'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>
       2 tarefas marcadas como : &quot;Categoria&ldquot; e &quot;termo&ldquot;
     </p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+            descricao={t.descricao}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
