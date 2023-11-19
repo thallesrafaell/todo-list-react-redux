4235abc4a4ed1870fe7947cb8caf77b15fba0380
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-19 16:56:38 -0300

    Criando Containers

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,9 @@
+const ListaDeTarefas = () => (
+  <main>
+    <p>
+      2 tarefas marcadas como : &quot;Categoria&ldquot; e &quot;termo&ldquot;
+    </p>
+  </main>
+)
+
+export default ListaDeTarefas
